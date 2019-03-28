933200ee66ac0b4f1a4b18a2b5dece566b2458b8
Roman Mxitaryan <clasharmenia0@gmail.com>
2019-03-28 13:31:50 +0400

    commited

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask, render_template,request, send_file
+from flask_socketio import SocketIO
+
+passwordTxt = open("password.txt", "r").read()
+controllPanelHTML = open("templates/controllPanel.html","r").read()
+explanationHTML = open("templates/explanation.html","r").read()
+connectUser = ""
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'vnkdjrtnfjknfl1232#'
+socketio = SocketIO(app)
+
+@app.route('/')
+def sessions():
+	return render_template('index.html')
+
+@app.route('/ArTarumianBarak-Regular-8925.ttf')
+def sendingFuckinFile():
+	return send_file("ArTarumianBarak-Regular-8925.ttf", mimetype='image/gif')
+
+@app.route('/bakcground.jpg')
+def sendingFuckinFile2():
+	return send_file("bakcground.jpg", mimetype='image/gif')
+
+@app.route('/logo.png')
+def sendingFuckinFile3():
+	return send_file("logo.png", mimetype='image/gif')
+
+@socketio.on('reviewOfPasswrd')
+def handle_my_custom_event(passwordS):
+	if passwordTxt == passwordS:
+		socketio.emit('answerOfPasswrd',controllPanelHTML,room = request.sid)
+		socketio.emit('connectedInRobot',{
+			'text':explanationHTML,
+			'id':request.sid
+		})
+		global connectUser
+		connectUser = request.sid
+		print(connectUser+" "+request.sid)
+	else:
+		socketio.emit('answerOfPasswrd',False,room = request.sid)
+
+
+@socketio.on('activateReview')
+def handle_my_custom_event(okok):
+	print(connectUser)
+	if connectUser != "":
+		socketio.emit('answerOfConnect',explanationHTML,room = request.sid)
+	else:
+		socketio.emit('answerOfConnect',False,room = request.sid)
+		
+if __name__ == '__main__':
+	socketio.run(app, debug=True)
