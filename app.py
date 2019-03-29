56584d1edeedad5f524e284a524d70e7011f5bd6
2002roman <40060590+2002roman@users.noreply.github.com>
2019-03-29 13:32:16 +0400

    Update app.py

--- app.py
+++ app.py
@@ -59,6 +59,7 @@ if __name__ == '__main__':
 from flask import Flask, render_template,request, send_file
 #from flask_socketio import SocketIO
 
+print("pkookok")
 app = Flask(__name__)
 #app.config['SECRET_KEY'] = 'vnkdjrtnfjknfl1232#'
 #socketio = SocketIO(app)
